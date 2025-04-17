65c6ae8e414375c2189fa5cd126fc3fab3d7e99a
dleitch00 <dleitch00@gmail.com>
2025-04-17 13:04:18 -0400

    final

--- 01_Blink/Blink.py
+++ 01_Blink/Blink.py
@@ -2,14 +2,17 @@ from gpiozero import LED as LEDClass # Alias
 import time
 
 LED = LEDClass(17)  # define led
+LED2 = LEDClass(12)
 
 def loop():
     global LED
     while True:
         LED.on() 
+        LED2.off()
         print ("led turned on >>>") # print information on terminal
         time.sleep(1)
         LED.off()
+        LED2.on()
         print ("led turned off <<<")
         time.sleep(1)
         
